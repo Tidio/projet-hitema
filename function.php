4bd8f4733e2ac2c71350098f0d94eb2192858e56
Edwarth <Demaret.steph@gmail.com>
2013-12-17 10:56:39 +0100

    Update function.php

--- function.php
+++ function.php
@@ -20,3 +20,31 @@ $stmt->execute();
 $news = $stmt->fetchAll();
 
 }
+
+
+//////// verifie si le login existe
+function veriflog($login,$mdp){
+
+	$stmt = $dbh->prepare("SELECT id,type
+	  FROM login
+	  WHERE login= :login
+	  AND passe= :mdp");
+	$stmt->bindValue(":login",$login);
+	$stmt->bindValue(":mdp",$mdp);
+	$stmt->execute();
+	$type_de_log = $stmt->fetchAll();
+	return $type_de_log;
+}
+
+//////// recupere l'id de l'utilisateur
+function recupid($id,$type){
+
+	$stmt = $dbh->prepare("SELECT *
+	  FROM :type
+	  WHERE id_login= :id");
+	$stmt->bindValue(":type",$type);
+	$stmt->bindValue("id",$id);
+	$stmt->execute();
+	$info_user = $stmt->fetchAll();
+	return $info_user;
+}
