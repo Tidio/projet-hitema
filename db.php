bae77d502f7719eb5ba7ad08c0c374d9eaae8ffa
sofiane92 <sosodu27@msn.com>
2013-12-17 09:55:16 +0100

    Update db.php

--- db.php
+++ db.php
@@ -3,7 +3,7 @@
 	define("DB_HOST", "localhost");
 	define("DB_USER", "root");
 	define("DB_PASSWORD", "");
-	define("DB_NAME", "archi");
+	define("DB_NAME", "hitema");
 
 	try {
 		$dbh = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASSWORD, array(
