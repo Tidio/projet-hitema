cf7a52798ae641a60c90290001e84d65c0d57037
Edwarth <Demaret.steph@gmail.com>
2013-12-17 12:51:56 +0100

    Create function_created.php

---
+++ function_created.php
@@ -0,0 +1,69 @@
+<?php
+require_once("db.php");
+
+function new_etudiant($nom,$prenom,$date,$adresse,$tel,$id_log){
+
+	$stmt = $dbh->prepare("INSERT INTO `eleve`( `nomel`, `prenomel`, `date_naissance`, `adresse`, `telephone`,id_login)
+						    VALUES (:nom,:prenom,:datee,:adresse,:tel,:id_log)");
+	$stmt->bindValue(":nom",$nom);
+	$stmt->bindValue(":prenom",$prenom);
+	$stmt->bindValue(":datee",$date);
+	$stmt->bindValue(":adresse",$adresse);
+	$stmt->bindValue(":tel",$tel);
+	$stmt->bindValue(":id_log",$id_log);
+	$stmt->execute();
+}
+
+function new_prof($nom,$prenom,$adresse,$tel,$id_log){
+
+	$stmt = $dbh->prepare("INSERT INTO `prof`( `nom`, `prenom`, `adresse`, `telephone`,id_login)
+						    VALUES (:nom,:prenom,:adresse,:tel,:id_log)");
+	$stmt->bindValue(":nom",$nom);
+	$stmt->bindValue(":prenom",$prenom);
+	$stmt->bindValue(":adresse",$adresse);
+	$stmt->bindValue(":tel",$tel);
+	$stmt->bindValue(":id_log",$id_log);
+	$stmt->execute();
+}
+
+function new_maitre_stage($nom,$tel,$id_log){
+
+	$stmt = $dbh->prepare("INSERT INTO `maitre_stage`( `nom`, `telephone`,id_login)
+						    VALUES (:nom,:prenom,:datee,:adresse,:tel,:id_log)");
+	$stmt->bindValue(":nom",$nom);
+	$stmt->bindValue(":tel",$tel);
+	$stmt->bindValue(":id_log",$id_log);
+	$stmt->execute();
+}
+
+function new_login($pseudo,$passe,$type){
+	$stmt = $dbh->prepare("INSERT INTO `login`( `pseudo`, `passe`, `type`) VALUES (:pseudo, :passe, :type)");
+	$stmt->bindValue(":pseudo",$pseudo);
+	$stmt->bindValue(":passe",$passe);
+	$stmt->bindValue(":type",$type);
+	$stmt->execute();
+	$id_log = $dbh->lastInsertId()
+
+	return $id_log;
+
+}
+
+
+function new_entreprise($nom_entreprise,$adresse_entreprise,$ville_entreprise,$cp_entreprise,$tel_entreprise){
+	$stmt = $dbh->prepare("INSERT INTO `entreprise`(`nom_entreprise`, `adresse`, `ville`, `cp`, `telephone`) 
+						    VALUES (:nom,:adresse,:ville,:cp,:tel)");
+	$stmt->bindValue(":nom",$nom_entreprise);
+	$stmt->bindValue(":adresse",$adresse_entreprise);
+	$stmt->bindValue(":ville",$ville_entreprise);
+	$stmt->bindValue(":cp",$cp_entreprise);
+	$stmt->bindValue(":tel",$tel_entreprise);
+	$stmt->execute();
+
+}
+
+
+
+
+
+
+?>
