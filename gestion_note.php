11accc3d9be9ac655c6ed4249d37f3160b51bd25
Tidio <tidiany.pro@gmail.com>
2013-12-18 13:53:48 +0100

    Update gestion_note.php

--- gestion_note.php
+++ gestion_note.php
@@ -13,17 +13,18 @@ function creerEval($eval){
 	$stmt->bindValue(':classe', $eval[4]);
 	$stmt->bindValue(':prof', $eval[5]);
 	$stmt->execute();
-	$lastid = $dbh->lastInsertId();
-	$sql ='SELECT id_eleve FROM ligne_classe WHERE id_classe=:classe';
+	$lastid = $dbh->lastInsertId($name='id_devoir');
+	$sql ="SELECT id_eleve FROM ligne_classe WHERE id_classe=:classe";
 	$stmt = $dbh->prepare($sql);
 	$stmt->bindValue(':classe', $eval[4]);
+	$stmt->execute();
 	$liste = $stmt->fetchAll();
 	foreach ($liste as $ideleve) {
 		$sql2 = "INSERT INTO evaluation value ('', :iddevoir, :ideleve, 0)";
 		$stmt2 = $dbh->prepare($sql2);
-		$stmt->bindValue(':iddevoir', $lastid);
-		$stmt->bindValue(':ideleve', $ideleve);
-		$stmt->execute();
+		$stmt2->bindValue(':iddevoir', $lastid);
+		$stmt2->bindValue(':ideleve', $ideleve['id_eleve']);
+		$stmt2->execute();
 	}
 }
 
@@ -40,11 +41,12 @@ function listeNote($idEval){
 
 function liste_nom_eval($idEval){
 		global $dbh;
-		$sql = 'SELECT nomel, prenomel, note
-			FROM evaluation, ligne_classe, eleve
-			WHERE id_eval=:ideval
+		$sql = 'SELECT nomel, prenomel, note, evaluation.id_eleve, id_eval
+			FROM devoir, evaluation, ligne_classe, eleve
+			WHERE evaluation.id_devoir = devoir.id_devoir
 			AND evaluation.id_eleve = ligne_classe.id_eleve
-			AND ligne_classe.id_eleve = eleve.id_eleve';
+			AND ligne_classe.id_eleve = eleve.id_eleve
+			AND evaluation.id_devoir=:ideval';
 		$stmt = $dbh->prepare($sql);
 		$stmt->bindValue(':ideval', $idEval);
 		$stmt->execute();
@@ -53,12 +55,29 @@ function liste_nom_eval($idEval){
 }
 
 //Modification d'une liste de note
-function modifNote(){
+function modif_note($note, $eleve, $eval){
+	global $dbh;
+	foreach ($note as $key => $n){
+		$sql = 'UPDATE evaluation SET note=:note WHERE id_eval =:ideval AND id_eleve=:ideleve';
+		$stmt = $dbh->prepare($sql);
+		$stmt->bindValue(':ideval', $eval);
+		$stmt->bindValue(':note', $n);
+		$stmt->bindValue(':ideleve', $eleve[$key]);
+		$stmt->execute();
+	}
+}
 
+//Supprimer un devoir
+function delete_eval($iddevoir){
+	global $dbh;
+	$sql = 'DELETE FROM devoir WHERE id_devoir =:iddevoir';
+	$stmt = $dbh->prepare($sql);
+	$stmt->bindValue(':iddevoir', $iddevoir);
+	$stmt->execute();
 }
 
 //Recupère une liste de note en fonction de critères
-function afficherNote(){
+function afficher_note(){
 
 }
 
@@ -70,4 +89,14 @@ function show_eval(){
 	$listeEval = $stmt->fetchAll();
 	return $listeEval;
 }
+
+function show_devoir(){
+	global $dbh;
+	$sql = 'SELECT id_devoir, nom_dev FROM devoir';
+	$stmt = $dbh->prepare($sql);
+	$stmt->execute();
+	$listeEval = $stmt->fetchAll();
+	return $listeEval;
+}
+
 ?>
