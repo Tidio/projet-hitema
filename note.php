f8fa93e05f66543ac7aa80cfc3f18ada33906507
Tidio <tidiany.pro@gmail.com>
2013-12-18 13:55:49 +0100

    Update note.php

--- note.php
+++ note.php
@@ -2,7 +2,11 @@
 session_start();
 require_once("db.php");
 require_once("gestion_note.php");
-$evaluations = show_eval();
+$devoir = show_devoir();
+
+if (isset($_POST['supprimer'])) {
+	delete_eval($_POST['evaluation']);
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -14,28 +18,42 @@ $evaluations = show_eval();
 <body>
 	<div>
 		<p>Evaluation: <p>
-		<select name="evaluation">
-			<?php foreach($evaluations as $eva) :?>
-			<option value="<?php echo $eva['id_matiere']?>"><?php echo $eva['nommat']?></option>
-			<?php endforeach; ?>
-		</select>
-		<a href="form_note.php"><input type="button" name="Creer" value="Créer"></a>
-		<br />
-		<table>
-			<tr>
-				<td>Nom</td>
-				<td>Prénom</td>
-				<td>Note</td>
-			</tr>
-			<?php if (!empty($_POST['evaluation'])){
-			$listeNom = liste_nom_eval($_POST['evaluation']);
-			foreach ($listeNote as $note): ?>
-				<tr>
-					<td><?php echo $note['nomel'] ?></td>
-					<td><?php echo $note['prenomel'] ?></td>
-					<td><?php echo $note['note'] ?></td>
-				</tr>
-			<?php endforeach; }?>
+			<form method="POST" action="note.php">
+				<select name="evaluation">
+					<?php foreach($devoir as $dev) :?>
+					<option value="<?php echo $dev['id_devoir']?>"><?php echo $dev['nom_dev']?></option>
+					<?php endforeach; ?>
+					</select>
+				<input type="submit" name="afficher" value="Afficher">
+				<input type="submit" name="supprimer" value="Supprimer">
+				<a href="form_note.php"><input type="button" name="Creer" value="Créer"></a>
+			
+			</form>
+			<br />
+			<form method="POST" action="changer_note.php">
+				<table>
+					<tr>
+						<td>Nom</td>
+						<td>Prénom</td>
+						<td>Note</td>
+					</tr>
+					<?php if (!empty($_POST['evaluation'])){
+					$listeNote = liste_nom_eval($_POST['evaluation']);
+					foreach ($listeNote as $note): ?>
+						<tr>
+							<td><?php echo $note['nomel'] ?></td>
+							<td><?php echo $note['prenomel'] ?></td>
+							<td><input type ="number" name="note[]" min="0" max="20" value="<?php echo $note['note'] ?>"></td>
+							<input type="hidden" name="eleve[]" value="<?php echo $note['id_eleve']?>">
+							<input type="hidden" name="eval" value="<?php echo $note['id_eval'] ?>"
+						</tr>
+					<?php endforeach; }?>
+
+				</table>
+				<?php if (!empty($_POST['evaluation'])): ?>
+						<input type="submit" value = "Sauvegarder">
+					<?php endif ?>
+			</form>
 	</div>
 </body>
 </html>
